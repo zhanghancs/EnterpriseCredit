95698b48a746475f59713cda08bcf8cc968d1905
dearsir1234 <3490404474@qq.com>
2023-09-06 08:55:05 +0800

    修改了市场总貌表格

--- src/main/java/com/example/enterprisecredit/service/impl/EnterpriseBasicInfoServiceImpl.java
+++ src/main/java/com/example/enterprisecredit/service/impl/EnterpriseBasicInfoServiceImpl.java
@@ -87,20 +87,69 @@ public class EnterpriseBasicInfoServiceImpl extends ServiceImpl<EnterpriseBasicI
     public List<EnterpriseBasicInfo> queryByIndex(String area, String transferMode, String industry) {
 
         QueryWrapper wrapper = new QueryWrapper();
-        if (area != null&&area!="All") {
+        if (area != null && !area.equals("All")) {
             wrapper.eq("area", area);
         }
-        if (transferMode != null&&transferMode!="All") {
+
+        if (transferMode != null && !transferMode.equals("All")) {
             wrapper.eq("transferMode", transferMode);
         }
-        if (industry != null&&industry!="All") {
+
+        if (industry != null && !industry.equals("All")) {
+            wrapper.eq("industry", industry);
+        }
+        wrapper.orderByDesc("income");
+        wrapper.last("LIMIT 200 ");
+        //2)执行查询
+        List<EnterpriseBasicInfo> EnterpriseBasicInfo = enterpriseBasicInfoMapper.selectList(wrapper);
+        System.out.println(EnterpriseBasicInfo.size());
+        return EnterpriseBasicInfo;
+
+    }
+    public List<List<EnterpriseBasicInfo>> queryTop150ByIncomeAndProfit(String area, String transferMode, String industry) {
+        QueryWrapper<EnterpriseBasicInfo> wrapper = new QueryWrapper<>();
+
+        if (area != null && !area.equals("All")) {
+            wrapper.eq("area", area);
+        }
+
+        if (transferMode != null && !transferMode.equals("All")) {
+            wrapper.eq("transferMode", transferMode);
+        }
+
+        if (industry != null && !industry.equals("All")) {
             wrapper.eq("industry", industry);
         }
 
+        // 按照income字段从大到小排序，限制最大返回行数为150
+        wrapper.orderByDesc("income").last("LIMIT 150");
+        List<EnterpriseBasicInfo> top150ByIncome = enterpriseBasicInfoMapper.selectList(wrapper);
 
-        //2)执行查询
-        List<EnterpriseBasicInfo> EnterpriseBasicInfo = enterpriseBasicInfoMapper.selectList(wrapper);
-        return EnterpriseBasicInfo;
+        // 清空wrapper，以便重新设置排序条件
+        QueryWrapper<EnterpriseBasicInfo> wrapper1 = new QueryWrapper<>();
+
+        if (area != null && !area.equals("All")) {
+            wrapper1.eq("area", area);
+        }
+
+        if (transferMode != null && !transferMode.equals("All")) {
+            wrapper1.eq("transferMode", transferMode);
+        }
+
+        if (industry != null && !industry.equals("All")) {
+            wrapper1.eq("industry", industry);
+        }
+
+        // 按照profit字段从大到小排序，限制最大返回行数为150
+        wrapper1.orderByDesc("profit").last("LIMIT 150");
+        List<EnterpriseBasicInfo> top150ByProfit = enterpriseBasicInfoMapper.selectList(wrapper1);
+
+        // 创建一个包含两个List的List
+        List<List<EnterpriseBasicInfo>> result = new ArrayList<>();
+        result.add(top150ByIncome);
+        result.add(top150ByProfit);
+
+        return result;
     }
 
     public List<EnterpriseBasicInfo> query2Enterprise(int stockCode1, int stockCode2)
